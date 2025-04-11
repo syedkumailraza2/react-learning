59890ee7e1be288e36a771a6bc983b2b785622f8
Syed Kumail Raza <syedkumailraza28@gmail.com>
2025-04-11 11:48:10 +0530

    basic react project created

---
+++ reactbasics/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Chai aur React | Syed Kumail Raza</h1>
+    </>
+  )
+}
+
+export default App
